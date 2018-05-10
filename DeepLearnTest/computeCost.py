fixed numerical stability issues tuned initing hypers randomly init weights: changed to based on activation function

DeepLearnTest/computeCost.py
@@ -9,10 +9,10 @@ def computeCost(y, yhat, debug=False, printall=False):
     if debug:
         print ("==Starting computeCost.py")
 
-    loss = np.sum ( -np.multiply (y, np.log(yhat) ), axis=0, keepdims=True )
+    #for numerical stability, add epsilon to yhat when close to 0
+    yhat[yhat<1e-200] = 1e-200
 
-    #m = y.shape[1]
-    #assert (loss.shape == (1,m))
+    loss = np.sum ( -np.multiply (y, np.log(yhat) ), axis=0, keepdims=True )
 
     if debug:
         print("loss.shape:",loss.shape)

DeepLearnTest/forwardProp.py
@@ -39,7 +39,9 @@ def forwardProp(L, params, activations, X, debug=False, printall=False):
         elif activation == "relu":
             al = np.fmax (zl, np.zeros(zl.shape, dtype="float64"))
         elif activation == "softmax":
-            al = np.exp (zl) / np.sum(np.exp (zl), axis=0, keepdims=True)
+            #for numerical stability, use "reduced" zl for exp
+            zl_temp = zl - np.max(zl, axis=0)
+            al = np.exp (zl_temp) / np.sum(np.exp (zl_temp), axis=0, keepdims=True)
         else:
             raise Exception("forwardProp.py failed: unknown activation")
 

DeepLearnTest/initHypersRand.py
@@ -1,13 +1,14 @@
 import numpy as np
 
-def initHypersRand():
+def initHypersRand(input_size):
     #Initializes hyperparameters in a random way
+    #   input_size - #units in input layer
     #
     # Returns: dictionary of hyperparameters with keys:
     #   learning_rate
     #   L - number of layers (excl. input)
     #   activations - activation functions numpy array, size L (last always softmax)
-    #   layerdims - dimensions of each layer, except input (size L). Last always 10 for digits
+    #   layerdims - dimensions of each layer, incl. input (size L+1). Last always 10 for digits
     #   optimization_technique - one of ["GradientDescent","GradientDescentWithMomentum","RMSProp","Adam"]
     #   beta_momentum - opt. techniques GradientDescentWithMomentum, Adam; optimization parameter beta1
     #   beta_rmsprop - opt. techniques RMSProp, Adam; optimization parameter beta2 (exp.weighted square averages)
@@ -17,17 +18,18 @@ def initHypersRand():
     #make sure it's random
     np.random.seed()
 
-    learning_rates = [1e-5, 3e-5, 1e-4, 3e-4, 1e-3, 3e-3, 1e-2, 3e-2, 1e-2]
+    learning_rates = [1e-4, 3e-4, 1e-3, 3e-3, 1e-2, 3e-2, 1e-2]
     hypers["learning_rate"] = learning_rates[np.random.randint(0,len(learning_rates))]
 
-    L_max = 3
-    L = np.random.randint(1, L_max+1) #1 to L_max
+    L_max = 4
+    L = np.random.randint(2, L_max+1) #2 to L_max
     hypers["L"] = L
 
     layerdims_max = 256
     layerdims_min = 10
     layerdims_last = 10
     layerdims = np.random.randint(layerdims_min, layerdims_max+1, L-1)
+    layerdims = np.append(input_size, layerdims)
     layerdims = np.append(layerdims, layerdims_last)
     hypers["layerdims"] = layerdims
 
@@ -37,7 +39,8 @@ def initHypersRand():
     activations = np.append(activations, activations_last)
     hypers["activations"] = activations
 
-    optimization_technique_all = np.array(["GradientDescent","GradientDescentWithMomentum","RMSProp","Adam"])
+    #optimization_technique_all = np.array(["GradientDescent","GradientDescentWithMomentum","RMSProp","Adam"])
+    optimization_technique_all = np.array(["RMSProp","Adam"])
     optimization_technique = optimization_technique_all[np.random.randint(0, len(optimization_technique_all))]
     hypers["optimization_technique"] = optimization_technique
 

DeepLearnTest/initWeights.py
@@ -1,10 +1,12 @@
 import numpy as np
 
-def initWeights(layerdims, alg="forRelu", debug=False):
+def initWeights(layerdims, activations, debug=False):
     #initializes weights 
     #   e.g. layerdims = [5,4,3], where 5 = #input nodes; 3=#output nodes
-    #   alg = "forRelu" = sqrt (2/n[l-1]) 
-    #       = "xavier" = sqrt (1/n[l-1])
+    #   activations: array size layerdims-1 (excl.input), elements one of the values:
+    #       "relu" - use forRelu algorithm: sqrt (2/n[l-1]) 
+    #       "tanh" - use "xavier" algorithm: sqrt (1/n[l-1])
+    #       for others (e.g. softmax) - what to use?
     #   Returns:
     #    dictionary (W1, b1, W2, b2, ..., WL, bL) , where L = len(layerdims)-1
     #       e.g. W1=[4,5] b1=[4,1],  W2=[4,3] b2=[3,1]
@@ -19,12 +21,13 @@ def initWeights(layerdims, alg="forRelu", debug=False):
         n_l = layerdims[layer]
         n_l_minus = layerdims[layer-1]
         temp = np.random.randn(n_l, n_l_minus)
-        if alg=="forRelu":
+        if activations[layer-1]=="relu":
             params["W"+str(layer)] = temp * np.sqrt(2./n_l_minus)
-        elif alg=="xavier":
+        elif activations[layer-1]=="tanh":
             params["W"+str(layer)] = temp * np.sqrt(1./n_l_minus)
         else:
-            raise Exception("initWeights.py failed: unknown alg")
+            params["W"+str(layer)] = temp * np.sqrt(2./n_l_minus)
+        #    raise Exception("initWeights.py failed: unknown alg")
         params["b"+str(layer)] = np.zeros((n_l,1), dtype="float64")
         if debug:
             #print ("weights prior to scaling:",temp)

DeepLearnTest/paramsResultsToFile.py
@@ -1,7 +1,21 @@
 import numpy as np
+import datetime
 
 def paramsResultsToFile(iter_count, L, params, activations, learning_rate,\
     optimization_technique,beta_momentum, beta_rmsprop, cost, accuracy):
+    #Output model parameters and results to a results.csv
+    #   iter_count - number of iterations model ran
+    #   L - number of layers excl. input
+    #   params - W's and b's of the model (not written to file)
+    #       layer_sizes - written to file from params' info
+    #   activations - activation function
+    #   learning_rate
+    #   optimization_technique: way of how params (W's and b's) are updated
+    #       one of ["GradientDescent","GradientDescentWithMomentum","RMSProp","Adam"]
+    #   beta_momentum: opt. techniques GradientDescentWithMomentum, Adam; optimization parameter beta1
+    #   beta_rmsprop: opt. techniques RMSProp, Adam; optimization parameter beta2 (exp.weighted square averages)
+    #   cost
+    #   accuracy 
 
     layer_sizes = np.zeros((L),'int')
     for layer in range(0, L):
@@ -9,7 +23,8 @@ def paramsResultsToFile(iter_count, L, params, activations, learning_rate,\
 
     file = open("..\\results.csv","a")
 
-    strToWrite = str(iter_count) + ";" + \
+    strToWrite = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ";" + \
+        str(iter_count) + ";" + \
         str(L) + ";" + \
         str(layer_sizes) + ";" + \
         str(activations) + ";" + \

DeepLearnTest/runModel.py
@@ -50,7 +50,8 @@ def runModel(iter_count, L, params, activations, X, y, learning_rate, minibach_s
         mline, = ax.plot([], costs, 'b-')
 
     for iter in range (iter_count):
-        print ("==========ITER:", str(iter))
+        if debug:
+            print ("==========ITER:", str(iter))
 
         #set current minibatch
         iterations_per_epoch = int (np.ceil ( m / minibach_size))
@@ -106,14 +107,16 @@ def runModel(iter_count, L, params, activations, X, y, learning_rate, minibach_s
     #print ("cc_time:",cc_time)
     #print ("bp_time:",bp_time)
     #print ("up_time:",up_time)
-    print ("Final cost:", cost)
-    print ("Final accuracy:", accuracy)
+    if debug:
+        print ("Final cost:", cost)
+        print ("Final accuracy:", accuracy)
 
     #Compute cost, accuracy for entire training data set
     if True:
         _, yhatFull = fp.forwardProp(L, params, activations, X, debug=False)
         costFull = cc.computeCost(y, yhatFull, debug=debug)
         accuracyFull = np.sum ( y [ np.argmax(yhatFull, axis=0), range(m) ] ) / m
+    if debug:
         print ("Final cost on full training set:", costFull)
         print ("Final accuracy on full training set:", accuracyFull)
 
@@ -126,4 +129,4 @@ def runModel(iter_count, L, params, activations, X, y, learning_rate, minibach_s
     #    plt.plot(range(len(costs)), costs)
     #    plt.show()
 
-    return za, yhat, grads, params
+    return yhatFull, grads, params